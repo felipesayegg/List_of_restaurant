ed20f5279f9bef26d30d95a466572531addef6ec
Felipe Sayeg <felipesayeg2018@gmail.com>
2024-03-24 17:20:44 -0300

    done

--- app/javascript/application.js
+++ app/javascript/application.js
@@ -3,3 +3,15 @@ import "@hotwired/turbo-rails"
 import "controllers"
 import "@popperjs/core"
 import "bootstrap"
+
+
+import { Application } from "stimulus";
+import { definitionsFromContext } from "stimulus/webpack-helpers";
+
+const application = Application.start();
+const context = require.context("../controllers", true, /\.js$/);
+application.load(definitionsFromContext(context));
+
+
+import "controllers/restaurants_api_controller";
+
